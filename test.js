b070d3bbd434787535379262fb62827b86a83cfe
”Haruka <9bqb9461@gmail.com>
2021-01-03 13:23:53 +0000

    for add to

---
+++ test.js
@@ -0,0 +1,7 @@
+function EVENT3() {
+    if( confirm ("OK(Yes) .Cancel(No)ボタン付きダイアログボックス") ) {
+        location.href="https://www.rakuteneagles.jp/"
+    }else{
+        location.href="test.html"
+    }
+}
